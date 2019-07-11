3670bc94b34a47c2536ff4ae32d88b1b35174c1a
yt8492 <tmymyut2000@gmail.com>
2019-07-11 23:46:44 +0900

    implement player

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    DiceGame().play()
+    println("What is your name?")
+    val playerName = readLine() ?: run {
+        println("Please input your name.")
+        return
+    }
+    val player = Player(playerName)
+    DiceGame(player).play()
 }
